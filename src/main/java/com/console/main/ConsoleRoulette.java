65fabf6ed6facd547ac0474327d0464e711fc0be
lodwinmoloto <lodwin@anylytical.co.za>
2021-01-20 23:12:41 +0200

    Implemented input file read capabilities

--- src/main/java/com/console/main/ConsoleRoulette.java
+++ src/main/java/com/console/main/ConsoleRoulette.java
@@ -1,10 +1,19 @@
 package com.console.main;
 
+import com.console.utilities.ReadInputFile;
 import com.console.utilities.UserInputs;
+
+import java.util.Scanner;
 
 public class ConsoleRoulette {
 
     public static void main(String[] Args){
+        Scanner scanner = new Scanner(System.in);
+
+        ReadInputFile.getPlayerNames();
+
+
+        scanner.nextLine();
 
         UserInputs.getUserInput();
 
--- src/main/java/com/console/utilities/ReadInputFile.java
+++ src/main/java/com/console/utilities/ReadInputFile.java
@@ -1,6 +1,28 @@
 package com.console.utilities;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class ReadInputFile {
+
+    public ReadInputFile() throws IOException {
+    }
+
+
+    public static void getPlayerNames(){
+        String path = "/Users/lodwinmoloto/Workplace/playerNames.txt";
+
+        try (Stream<String> stream = Files.lines(Paths.get(path))){
+            stream.forEach(System.out::println);
+
+            System.out.println("Press Enter to proceed");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
