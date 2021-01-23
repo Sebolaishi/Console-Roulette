44d3a5cf17c76f1b3af1a34c002df62364889be7
lodwinmoloto <lodwin@anylytical.co.za>
2021-01-24 00:23:16 +0200

    ctok, mtok, ktom functionalities unit tests

--- src/test/java/ConversionsTests/ConversionsTests.java
+++ src/test/java/ConversionsTests/ConversionsTests.java
@@ -16,6 +16,8 @@ public class ConversionsTests {
      */
     private static final float KELVIN_INPUT = 100;
     private static final float CELSIUS_INPUT = 100;
+    private static final double MILES = 100;
+    private static final double KILOMETERS = 100;
 
     /**
      * Methods
@@ -57,18 +59,47 @@ public class ConversionsTests {
     }
 
     /**
-     * Test kelvin to celsius functionality
+     * Test celsius to kelvins functionality
      */
     @Test
     public void ctokTest(){
-        float celsius = Conversions.ktoc(CELSIUS_INPUT);
-        System.out.println("Celsius Value is : " + celsius);
+        float kelvin = Conversions.ktoc(CELSIUS_INPUT);
+        System.out.println("Kelvin Value is : " + kelvin);
 
         /**
          * Redundant null check because float is a primitive data type thus its never null
          * just had it for testing purpose
          */
-        Assert.assertNotNull(celsius);
+        Assert.assertNotNull(kelvin);
     }
 
+    /**
+     * Test miles to kilometers functionality
+     */
+    @Test
+    public void mtokTest(){
+        double kilometers = Conversions.mtok(MILES);
+        System.out.println("Kilometers Value is : " + kilometers);
+
+        /**
+         * Redundant null check because float is a primitive data type thus its never null
+         * just had it for testing purpose
+         */
+        Assert.assertNotNull(kilometers);
+    }
+
+    /**
+     * Test miles to kilometers functionality
+     */
+    @Test
+    public void ktomTest(){
+        double miles = Conversions.ktom(KILOMETERS);
+        System.out.println("Miles Value is : " + miles);
+
+        /**
+         * Redundant null check because float is a primitive data type thus its never null
+         * just had it for testing purpose
+         */
+        Assert.assertNotNull(miles);
+    }
 }
